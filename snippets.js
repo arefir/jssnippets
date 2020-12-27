8cf6c50beb4e0a4743439de14a2e5c63ab16d610
arefir <afirdaus.razak11@gmail.com>
2020-12-28 01:31:49 +0800

    remove console.log

--- snippets.js
+++ snippets.js
@@ -19,20 +19,9 @@ food.map((x, y) => {
   }
 });
 
-// console.log(samefood);
-
 let count = 0;
 
 samefood.forEach((x) => {
-  // console.log("foreach " + x);
   food.splice(x - count, 1);
-  // samefood = samefood.map((x) => {
-  //   console.log("map " + x);
-  //   const y = x - 1;
-  //   return y;
-  // });
-  // console.log("samefood after tolak" + samefood);
   count++;
 });
-
-//
